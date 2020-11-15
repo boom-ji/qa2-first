94cd9b4b72705cf0aaa75436ed3b1c5b2e180f9e
Lena Tripathi <lena@Animeshs-MacBook.local>
2020-11-15 13:47:21 +0200

    Initial commit

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+    private String name = "Vladika";
+
+
+    @Test
+    public void firstTest() {
+        System.out.println("Hello, Corona! Corona go away!");
+        System.out.println("Corona go away!");
+
+        int sum = sumTwoDigits( 2001, 19);
+        System.out.println("Away from: " + sum);
+
+        System.out.println(sumTwoDigits(2000, 21));
+        System.out.println(name);
+
+
+    }
+
+    private int sumTwoDigits(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+}
