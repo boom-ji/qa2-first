c6402bd239f8d50af635a1b406b3094c82674ec8
boom-ji <boomjoom77>
2021-01-03 15:09:10 +0200

    First full test with class (to page obj)

---
+++ src/test/java/JavaObjectWorkshop.java
@@ -0,0 +1,45 @@
+import model.Course;
+import model.Student;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaObjectWorkshop {
+    @Test
+    public void workWithObjects(){
+        Student lena = new Student();
+
+        lena.setFirstName("Lena");
+        lena.setLastName("Mandarinkova");
+        lena.setAge(18);
+        lena.setEmail("lapusjka@inbox.lv");
+
+
+        Student raimonds = new Student("Raimponds", "Gorillins", 19, "sexyboy@gmail.com");
+
+//        raimonds.setFirstName("Raimonds");
+//        raimonds.setLastName("Geislers");
+//        raimonds.setAge(19);
+//        raimonds.setEmail("sexyboy@gmail.com");
+
+//        System.out.println(lena.getFirstName() + " " + lena.getLastName());
+//        System.out.println(lena.getFullName());
+//        System.out.println(raimonds.getFirstName() + " " + raimonds.getLastName());
+//        System.out.println(raimonds.getFullName());
+
+        List<Student> students = new ArrayList<Student>();
+        students.add(lena);
+        students.add(raimonds);
+        students.add(new Student("Liga", "BlaBla", 18, "liga17@gkl.com"));
+
+        Course qa2 = new Course("QA2 - Automation", 3, students);
+        qa2.addStudent(new Student("Dmitrijs", "Alibaba", 100500, "barola@gmia.lv"));
+
+
+
+        qa2.printInfo();
+
+
+    }
+}
