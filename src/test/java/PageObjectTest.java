7629c2512ed05a3636ea704cce48e2652c7d7516
boom-ji <boomjoom77>
2021-02-23 13:14:10 +0200

    QA2 21.02.21

--- src/test/java/PageObjectTest.java
+++ src/test/java/PageObjectTest.java
@@ -1,15 +1,42 @@
+import org.apache.logging.log4j.LogManager;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import pages.ArticlePage;
 import pages.BaseFunc;
 import pages.HomePage;
+import sun.util.resources.ga.LocaleNames_ga;
+
+import java.util.logging.Logger;
 
 public class PageObjectTest {
+    private final Logger LOGGER = LogManager.getLogger(this.getClass());
+    private int articleId = 3;
+
     @Test
     public void tvnetPageObjectTest(){
+        LOGGER.info("This test is checking article titles and comment counts");
+
         BaseFunc baseFunc = new BaseFunc();
         baseFunc.openUrl("http://tvnet.lv");
 
         HomePage homePage = new HomePage(baseFunc);
         homePage.acceptCookies();
-        homePage.openArticleById(3);
+        //Get article title on Home page
+//        String homePageTitle = homePage.getArticleById(articleId);
+        // ?????   String homePageTitle = HomePage.getArticleTitleById(articleId);
+
+        homePage.openArticleById(articleId);
+
+//       ArticlePage articlePage = new ArticlePage(baseFunc);
+        ArticlePage articlePage = homePage.openArticleById(articleId);
+        //Get article title on Article page
+        String articlePageTitle = articlePage.getTitle();
+
+
+        //Compare article titles
+//        Assertions.assertEquals(homePageTitle, articlePageTitle, "Titles are not the same");
+        Assertions.assertTrue(homePageTitle.startsWith(articlePageTitle), "Titles are not the same");
+
+
     }
 }
--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,21 +1,40 @@
 package pages;
 
+import helpers.CommentsHelper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import sun.security.ssl.SSLLogger;
+
+import java.util.logging.Logger;
 
 public class HomePage {
-    private final By ACCEPT_BTN = By.xpath(".//div[contains(@class, 'button cookie')]");
+    private final By ACCEPT_BTN = By.xpath(".//button[@mode = 'primary']");
     private final By TITLE = By.xpath(".//span[@itemprop = 'headline name']");
+    private final By COMMENTS_COUNT = By.xpath(".//span[contains(@class, '__comment')]");
     private BaseFunc baseFunc;
+
 
     public  HomePage(BaseFunc baseFunc) {
         this.baseFunc = baseFunc;
     }
-    public void acceptCookies() {
-        baseFunc.click(ACCEPT_BTN);
+
+    public void acceptCookies() { baseFunc.click(ACCEPT_BTN);
     }
-    public void openArticleById(int id) {
+    public ArticlePage openArticleById(int id) {
         WebElement articleToClick = baseFunc.findElements(TITLE).get(id);
         baseFunc.click(articleToClick);
+        return new ArticlePage(baseFunc);
+    }
+
+    public String getArticleById(int id) {
+        Logger.info("Getting article Nr." + id + " title");
+
+        return baseFunc.getText(TITLE, id);
+    }
+
+    public int getCommentCountById() {
+        CommentsHelper helper = new CommentsHelper();
+        return helper.getCommentCount(baseFunc.findElement(COMMENTS_COUNT));
+
     }
 }
